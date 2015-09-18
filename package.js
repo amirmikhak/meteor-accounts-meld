5675cc31a1b1266ef211f923255d7d7bf5701a1e
Amir Mikhak <amirmikhak@notidle.com>
2015-09-18 14:40:02 -0400

    update version number

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
 	summary: "Meteor package to link/meld user accounts registered " +
 	         "with the same (verified) email address.",
-  version: "1.3.1",
+  version: "1.3.2",
   name: "amirmikhak:accounts-meld",
   git: "https://github.com/amirmikhak/meteor-accounts-meld.git",
 });
